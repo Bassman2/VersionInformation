21967ade2a349e724cca4a2639e73b93b8de7a35
Bassman2 <ralf.beckers@hotmail.com>
2021-09-22 13:42:02 +0200

    update

--- VersionInformation/Program.cs
+++ VersionInformation/Program.cs
@@ -6,7 +6,38 @@ namespace VersionInformation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            switch (args[0])
+            {
+            case "show": 
+                ShowVersionInformation(args[1]); 
+                break;
+            case "set": 
+                SetVersionInformation(args[1], args[2]); 
+                break;
+            default:
+                Console.WriteLine("VersionInformation");
+                Console.WriteLine("Copyright 2021 Ralf Beckers");
+                Console.WriteLine();
+                Console.WriteLine("VersionInformation show <Solution.sln>");
+                Console.WriteLine("VersionInformation set <Solution.sln> <version.xml>");
+                break;
+            }
+        }
+
+        private static void ShowVersionInformation(string solutionFile)
+        { 
+            foreach (var project in SolutionParser.Parse(solutionFile))
+            {
+                Console.WriteLine(project);
+            }
+        }
+
+        private static void SetVersionInformation(string solutionFile, string versionFile)
+        {
+            foreach (var project in SolutionParser.Parse(solutionFile))
+            {
+
+            }
         }
     }
 }
---
+++ VersionInformation/SolutionParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VersionInformation
+{
+    public static class SolutionParser
+    {
+        //Project("{D954291E-2A0B-460D-934E-DC6B0785DB48}") = "MediaDevicesShare", "Src\MediaDevicesShare\MediaDevicesShare.shproj", "{8C6734B5-5BBB-413E-A5F2-FC7E6AAFFCE7}"
+
+        public static List<string> Parse(string solutionPath)
+        {
+            List<string> projects = new();
+            using var reader = File.OpenText(solutionPath);
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (line.StartsWith("Project"))
+                {
+                    string[] items = line.Split(',');
+                    if (items.Length >= 3)
+                    {
+                        string project = items[1].Trim(' ', '"');
+
+                        if (project.EndsWith(".csproj"))
+
+                        projects.Add(project);
+                    }
+                }
+            }
+            return projects;
+        }
+    }
+}
