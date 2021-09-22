4931b61f01d4a916b5af76032f46c3e4320d397d
Bassman2 <ralf.beckers@hotmail.com>
2021-09-22 13:02:52 +0200

    update

---
+++ VersionInformation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VersionInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
